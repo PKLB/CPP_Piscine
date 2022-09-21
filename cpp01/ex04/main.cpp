ef82476555885bba2818202d10b76c778da8bd66
Paco Le berre <pacocmoi@live.fr>
2022-09-21 14:37:30 +0200

    sed not done hihihihi

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -1,36 +1,8 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
 using std::string;
-
-// int main(int argc, char *argv[])
-// {
-// 	if (argc == 4)
-// 	{
-// 		string document = argv[1];
-// 		string outdocument = document + ".replace";
-// 		string s1 = argv[2];
-// 		string s2 = argv[3];
-
-// 		std::ifstream infile(document);
-// 		std::ofstream outfile(outdocument);
-// 		if (infile.is_open())
-// 		{
-// 			std::cout << "We opened the document " << argv[1] << std::endl;
-// 			std::string buffer;
-// 			while (infile >> buffer)
-// 			{
-// 				if (s1 == buffer)
-// 				{
-// 					outfile << s2;
-// 					std::cout << "found matching words\n";
-// 				}
-// 				else
-// 					outfile << buffer;
-// 			}
-// 		}
-// 	}
-// }
 
 int main(int argc, char *argv[])
 {
@@ -40,6 +12,9 @@ int main(int argc, char *argv[])
 		string filenameReplace = filename + ".replace";
 		string s1 = argv[2];
 		string s2 = argv[3];
-		
+		std::ifstream infile(filename);
+		std::stringstream tmp;
+		tmp << infile.rdbuf();
+		string tmp2 = tmp.str();
 	}
 }
