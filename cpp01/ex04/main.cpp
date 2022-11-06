c6c637b3431eb5579b0c17675b2bb2752ae64d84
paco <pacocmoi@live.fr>
2022-11-06 18:39:18 +0100

    forgot u sry ex04

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -3,35 +3,6 @@
 #include <string>
 using std::string;
 
-// int main(int argc, char *argv[])
-// {
-// 	if (argc == 4)
-// 	{
-// 		string document = argv[1];
-// 		string outdocument = document + ".replace";
-// 		string s1 = argv[2];
-// 		string s2 = argv[3];
-
-// 		std::ifstream infile(document);
-// 		std::ofstream outfile(outdocument);
-// 		if (infile.is_open())
-// 		{
-// 			std::cout << "We opened the document " << argv[1] << std::endl;
-// 			std::string buffer;
-// 			while (infile >> buffer)
-// 			{
-// 				if (s1 == buffer)
-// 				{
-// 					outfile << s2;
-// 					std::cout << "found matching words\n";
-// 				}
-// 				else
-// 					outfile << buffer;
-// 			}
-// 		}
-// 	}
-// }
-
 int main(int argc, char *argv[])
 {
 	if (argc == 4)
@@ -45,9 +16,21 @@ int main(int argc, char *argv[])
 		std::ofstream outfile(outdocument, std::ios::binary);
 
 		string word;
-		string sentence;
+		string result;
 		while (getline(infile, word))
-			sentence += word;
-		std::cout << sentence << std::endl;
+			result += word;
+		std::cout << result << std::endl;
+		int	s1_length = s1.length();
+		for (int index = 0; index < result.length(); index++)
+		{
+			if (result.compare(index, s1_length, s1) == 0)
+			{
+				result.erase(index, s1_length);
+				result.insert(index, s2);
+			}
+		}
+		std::cout << result << std::endl;
+		outfile << result;
+   		outfile.close();
 	}
 }
