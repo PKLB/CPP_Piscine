c7a9a310dc68b1c30049a1bf5c431dc46c4a1688
paco <pacocmoi@live.fr>
2022-11-27 22:09:15 +0100

    cpp02 isn't done yet

--- cpp02/ex00/Fixed.cpp
+++ cpp02/ex00/Fixed.cpp
@@ -6,7 +6,30 @@ Fixed::Fixed() :number(0)
 	std::cout << "Default constructor called\n";
 }
 
+Fixed::Fixed(Fixed const &src){
+	std::cout << "Copy constructor called\n";
+	*this = src;
+}
+
 Fixed::~Fixed()
 {
 	std::cout << "Destructor called\n";
+}
+
+Fixed &Fixed::operator=(const Fixed& src)
+{
+	std::cout << "Copy assignment operator called\n";
+	this->number = src.getRawBits();
+	return *this;
+}
+
+int Fixed::getRawBits() const
+{
+	std::cout << "getRawBits member function called\n";
+	return this->number;
+}
+
+void Fixed::setRawBits(int const raw)
+{
+	this->number = raw;
 }
