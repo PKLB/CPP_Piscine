ea8eb67f49e82cdbd9df2a75b117d830be9b95e3
Paco Le berre <pacocmoi@live.fr>
2023-05-19 17:23:51 +0200

    ex02 is done siu

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -3,16 +3,88 @@
 PmergeMe::PmergeMe(){
 }
 
+std::vector<int> PmergeMe::ford_johnson_vector(std::vector<int> tmp_vector){
+	std::vector<int> sorted_vector;
+	std::vector<int> left_part;
+	std::vector<int> right_part;
+
+	if (tmp_vector.size() <= 1)
+		return tmp_vector;
+	else{
+		for (struct {std::vector<int>::iterator it; size_t index;} s = {tmp_vector.begin(), 0}; s.it != tmp_vector.end(); s.it++, s.index++)
+			(s.index <= tmp_vector.size() / 2 ? left_part : right_part).push_back((*s.it));
+		sorted_vector = fusion_vector(left_part, right_part);
+	}
+	if (!std::is_sorted(sorted_vector.begin(), sorted_vector.end()))
+		sort(sorted_vector.begin(), sorted_vector.end());
+	std::unique(sorted_vector.begin(), sorted_vector.end());
+
+	return (sorted_vector);
+}
+
+std::vector<int> PmergeMe::fusion_vector(std::vector<int> left_part, std::vector<int> right_part){
+	std::vector<int> fusioned_vector;
+	std::vector<int>::iterator left_it = left_part.begin();
+	std::vector<int>::iterator right_it = right_part.begin();
+	while (left_it != left_part.end() && right_it != right_part.end())
+	{
+		if ((*left_it) <= (*right_it))
+			fusioned_vector.push_back((*left_it++));
+		else
+			fusioned_vector.push_back((*right_it++));
+	}
+	for (std::vector<int>::iterator it = left_it ;it != left_part.end(); it++)
+		fusioned_vector.push_back((*it));
+	for (std::vector<int>::iterator it = right_it;it != right_part.end(); it++)
+		fusioned_vector.push_back((*it));
+
+	return fusioned_vector;
+}
+
+std::list<int> PmergeMe::ford_johnson_list(std::list<int> tmp_list)
+{
+	std::list<int> sorted_list;
+	std::list<int> left_part;
+	std::list<int> right_part;
+
+	if (tmp_list.size() <= 1)
+		return tmp_list;
+	else{
+		for (struct {std::list<int>::iterator it; size_t index;} s = {tmp_list.begin(), 0}; s.it != tmp_list.end(); s.it++, s.index++)
+			(s.index <= tmp_list.size() / 2 ? left_part : right_part).push_back((*s.it));
+		sorted_list = fusion_list(left_part, right_part);
+	}
+	if (!std::is_sorted(sorted_list.begin(), sorted_list.end()))
+		sorted_list.sort();
+	sorted_list.unique();
+	return (sorted_list);
+}
+
+std::list<int> PmergeMe::fusion_list(std::list<int> left_part, std::list<int> right_part)
+{
+	std::list<int> fusioned_list;
+	std::list<int>::iterator left_it = left_part.begin();
+	std::list<int>::iterator right_it = right_part.begin();
+	while (left_it != left_part.end() && right_it != right_part.end())
+	{
+		if ((*left_it) <= (*right_it))
+			fusioned_list.push_back((*left_it++));
+		else
+			fusioned_list.push_back((*right_it++));
+	}
+	for (std::list<int>::iterator it = left_it ;it != left_part.end(); it++)
+		fusioned_list.push_back((*it));
+	for (std::list<int>::iterator it = right_it;it != right_part.end(); it++)
+		fusioned_list.push_back((*it));
+	return fusioned_list;
+}
+
 void PmergeMe::sort_list(std::list<int>& listo){
 	int length = listo.size();
 	std::list<int>::iterator it;
  	struct timeval begin, end;
     gettimeofday(&begin, 0);
-
-	// listo.sort();
-	/* list.sort() some kind of merge sort() implemented in list, subject specifies this : "It is strongly advised to implement
-	your algorithm for each container and thus to avoid using a generic function." So it's not forbidden, but this sort is a merge sort
-	so it might not fit the requirements? */
+	listo = ford_johnson_list(listo);
     gettimeofday(&end, 0);
 	std::cout << "\033[1;36mAfter: \033[0m";
 	for (it = listo.begin(); it != listo.end(); ++it)
@@ -32,14 +104,12 @@ void PmergeMe::sort_vector(std::vector<int>& vectoro){
 	int length = vectoro.size();
 	struct timeval begin, end;
     gettimeofday(&begin, 0);
-
-	// std::sort (vectoro.begin(), vectoro.end());
-	// insertionSort(vectoro);
+	vectoro = ford_johnson_vector(vectoro);
 	gettimeofday(&end, 0);
-	std::cout << "[vector] After: ";
-	for (size_t i = 0; i < vectoro.size(); i++)
-		std::cout << vectoro[i] << " ";
-	std::cout << "\n";
+	// std::cout << "[vector] After: ";
+	// for (size_t i = 0; i < vectoro.size(); i++)
+	// 	std::cout << vectoro[i] << " ";
+	// std::cout << "\n";
 
 	long seconds = end.tv_sec - begin.tv_sec;
     long microseconds = end.tv_usec - begin.tv_usec;
--- cpp09/ex02/PmergeMe.hpp
+++ cpp09/ex02/PmergeMe.hpp
@@ -6,6 +6,7 @@
 #include <sstream>
 #include <iostream>
 #include <iterator>
+#include <algorithm>
 #include <ctime>
 #include <sys/time.h>
 
@@ -15,6 +16,10 @@ class PmergeMe {
 		void sort_list(std::list<int>& listo);
 		void sort_vector(std::vector<int>& vectoro);
 		int check_data(const std::string input);
+		std::vector<int> ford_johnson_vector(std::vector<int>);
+		std::vector<int> fusion_vector(std::vector<int> left_part, std::vector<int> right_part);
+		std::list<int> ford_johnson_list(std::list<int>);
+		std::list<int> fusion_list(std::list<int> left_part, std::list<int> right_part);
 		PmergeMe(const PmergeMe& src);
 		~PmergeMe();
 		PmergeMe& operator=(const PmergeMe& src);
