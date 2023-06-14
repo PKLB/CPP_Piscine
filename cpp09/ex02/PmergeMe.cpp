047af4dd5eb2ccc04eb9b646599348e8eb29f112
Paco Le berre <pacocmoi@live.fr>
2023-06-14 17:13:06 +0200

    fixed RPN, changed algo of ex02  to ford johnson, need to sort lists and it should be done

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -3,135 +3,61 @@
 PmergeMe::PmergeMe(){
 }
 
-std::vector<int> PmergeMe::fusion_vector(std::vector<int> sorted_vector, std::vector<int> tmp_vector){
-	std::vector<int>::iterator it1 = sorted_vector.begin();
-	std::vector<int>::iterator it2 = tmp_vector.begin();
-	std::vector<int> final_vector;
+void merge(std::vector<int>& left, std::vector<int>& right, std::vector<int>& result) {
+    std::vector<int>::iterator leftIt = left.begin();
+    std::vector<int>::iterator rightIt = right.begin();
 
-	while(it1 != sorted_vector.end() && it2 != tmp_vector.end())
-	{
-		if ((*it1) < (*it2)){
-			final_vector.push_back((*it1));
-			it1++;
-		}
-		else{
-			final_vector.push_back((*it2));
-			it2++;
-		}
-	}
-	while (it1 != sorted_vector.end()) {
-		final_vector.push_back((*it1));
-		++it1;
-	}
-	while (it2 != tmp_vector.end()) {
-		final_vector.push_back((*it2));
-		++it2;
-	}
-	return final_vector;
+    while (leftIt != left.end() && rightIt != right.end()) {
+        if (*leftIt <= *rightIt) {
+            result.push_back(*leftIt);
+            ++leftIt;
+        } else {
+            result.push_back(*rightIt);
+            ++rightIt;
+        }
+    }
+    while (leftIt != left.end()) {
+        result.push_back(*leftIt);
+        ++leftIt;
+    }
+    while (rightIt != right.end()) {
+        result.push_back(*rightIt);
+        ++rightIt;
+    }
 }
 
-std::list<int> PmergeMe::fusion_list(std::list<int> sorted_list, std::list<int> tmp_list){
-	std::list<int>::iterator it1 = sorted_list.begin();
-	std::list<int>::iterator it2 = tmp_list.begin();
-	std::list<int> final_list;
-
-	while(it1 != sorted_list.end() && it2 != tmp_list.end())
-	{
-		if ((*it1) < (*it2)){
-			final_list.push_back((*it1));
-			it1++;
-		}
-		else{
-			final_list.push_back((*it2));
-			it2++;
-		}
-	}
-	while (it1 != sorted_list.end()) {
-		final_list.push_back((*it1));
-		++it1;
-	}
-	while (it2 != tmp_list.end()) {
-		final_list.push_back((*it2));
-		++it2;
-	}
-
-	return final_list;
+void ford_johnson(std::vector<int>& vectoro) {
+    if (vectoro.size() <= 1) {
+        return;
+    }
+    std::vector<int> left;
+    std::vector<int> right;
+    std::vector<int> sorted;
+    std::vector<int>::iterator middle = vectoro.begin() + (vectoro.size() / 2);
+    left.assign(vectoro.begin(), middle);
+    right.assign(middle, vectoro.end());
+    ford_johnson(left);
+    ford_johnson(right);
+    merge(left, right, sorted);
+    std::vector<int> remaining(vectoro.begin(), middle);
+    // std::sort(remaining.begin(), remaining.end(), std::greater<int>());
+    for (std::vector<int>::iterator it = remaining.begin(); it != remaining.end(); ++it) {
+        std::vector<int>::iterator insertPos = std::lower_bound(sorted.begin(), sorted.end(), *it);
+        sorted.insert(insertPos, *it);
+    }
+    vectoro = sorted;
 }
 
-std::vector<int> PmergeMe::algo_sort_vector(std::vector<int> tmp_vector){
-	std::vector<int> sorted_vector;
-
-	if (tmp_vector.size() <= 1)
-		return tmp_vector;
-	else{
-		for (std::vector<int>::iterator tmp = tmp_vector.begin(); tmp != tmp_vector.end(); tmp++){
-			std::vector<int> tmp_vector(1, *tmp);
-			sorted_vector = fusion_vector(sorted_vector, tmp_vector);
-		}
-	}
-	if (!std::is_sorted(sorted_vector.begin(), sorted_vector.end()))
-		algo_sort_vector(sorted_vector);
-	return (sorted_vector);
-}
-
-std::list<int> PmergeMe::algo_sort_list(std::list<int> tmp_list)
-{
-	std::list<int> sorted_list;
-
-	if (tmp_list.size() <= 1)
-		return tmp_list;
-	else{
-		for (std::list<int>::iterator tmp = tmp_list.begin(); tmp != tmp_list.end(); tmp++){
-			std::list<int> tmp_list(1, *tmp);
-			sorted_list = fusion_list(sorted_list, tmp_list);
-	}
-	}
-	if (!std::is_sorted(sorted_list.begin(), sorted_list.end()))
-		sorted_list.sort();
-	sorted_list.unique();
-	return (sorted_list);
-}
-
-
-void PmergeMe::sort_list(std::list<int>& listo){
-	int length = listo.size();
-	std::list<int>::iterator it;
- 	struct timeval begin, end;
-    gettimeofday(&begin, 0);
-	listo = algo_sort_list(listo);
-    gettimeofday(&end, 0);
-	// std::cout << "\033[1;36mAfter: \033[0m";
-	// for (it = listo.begin(); it != listo.end(); ++it)
-	// 	std::cout << *it << " ";
-	// std::cout << "\n";
-
-    long seconds = end.tv_sec - begin.tv_sec;
-    long microseconds = end.tv_usec - begin.tv_usec;
-    double elapsed = seconds + microseconds * 1e-6;
-	std::cout << "Time to process a range of " << length <<
-		" elements with \033[1;32mstd::list\033[0m: ";
-	std::cout << std::setprecision(6) << std::fixed << elapsed << "s" << std::endl;
-	return;
-}
-
-void PmergeMe::sort_vector(std::vector<int>& vectoro){
-	int length = vectoro.size();
-	struct timeval begin, end;
-    gettimeofday(&begin, 0);
-	vectoro = algo_sort_vector(vectoro);
-	gettimeofday(&end, 0);
-	std::cout << "\033[1;33mAfter:  \033[0m";
-	for (size_t i = 0; i < vectoro.size(); i++)
-		std::cout << vectoro[i] << " ";
-	std::cout << "\n";
-
-	long seconds = end.tv_sec - begin.tv_sec;
-    long microseconds = end.tv_usec - begin.tv_usec;
-    double elapsed = seconds + microseconds * 1e-6;
-	std::cout << "Time to process a range of " << length <<
-		" elements with \033[1;32mstd::vector\033[0m: ";
-	std::cout << std::setprecision(6) << std::fixed << elapsed << "s" << std::endl;
-	return;
+void  PmergeMe::do_algo(std::vector<int> vectoro) {
+    ford_johnson(vectoro);
+    std::cout << "\033[1;33mAfter: \033[0m: ";
+	int tmp;
+    for (std::vector<int>::iterator it = vectoro.begin(); it != vectoro.end(); ++it) {
+        if (tmp != *it)
+			std::cout << *it << " ";
+		tmp = *it;
+    }
+    std::cout << std::endl;
 }
 
 int PmergeMe::check_data(std::string input) {
--- cpp09/ex02/PmergeMe.hpp
+++ cpp09/ex02/PmergeMe.hpp
@@ -11,16 +11,11 @@
 #include <sys/time.h>
 
 class PmergeMe {
-	public:
-		PmergeMe();
-		void sort_list(std::list<int>& listo);
-		void sort_vector(std::vector<int>& vectoro);
-		int check_data(const std::string input);
-		std::vector<int> algo_sort_vector(std::vector<int>);
-		std::vector<int> fusion_vector(std::vector<int> sorted_vector, std::vector<int> tmp_vector);
-		std::list<int> algo_sort_list(std::list<int>);
-		std::list<int> fusion_list(std::list<int> sorted_list, std::list<int> tmp_list);
-		PmergeMe(const PmergeMe& src);
-		~PmergeMe();
-		PmergeMe& operator=(const PmergeMe& src);
+public:
+    PmergeMe();
+	int check_data(std::string input);
+	void do_algo(std::vector<int> arr);
+    PmergeMe(const PmergeMe& src);
+    ~PmergeMe();
+    PmergeMe& operator=(const PmergeMe& src);
 };
